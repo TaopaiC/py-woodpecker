bd9b44c0f6bcd645acdfcdbb8b7dcf0cc8199887
TaopaiC <124380+TaopaiC@users.noreply.github.com>
2022-07-28 09:34:07 +0800

    add threading job

--- woodpecker.py
+++ woodpecker.py
@@ -1,12 +1,16 @@
 import requests
 import time
 import argparse
+import threading
 from urllib3.connectionpool import HTTPConnectionPool
 
 parser = argparse.ArgumentParser(description='woodpecker')
 parser.add_argument("--url", type=str, default='https://httpbin.org/get', help="url")
-parser.add_argument("--start", type=int, default=20, help="interval start at (sec)")
+parser.add_argument("--init", type=int, default=20, help="interval init at (sec)")
 parser.add_argument("--step", type=int, default=10, help="interval step (sec)")
+parser.add_argument("--job-enable", type=bool, default=False, help="enable 2nd threading job")
+parser.add_argument("--job-init", type=int, default=20, help="for 2nd job, interval init at (sec)")
+parser.add_argument("--job-step", type=int, default=10, help="for 2nd job, interval step (sec)")
 args = parser.parse_args()
 print(args)
 
@@ -23,7 +27,7 @@ def patch_connectionpool():
 patch_connectionpool()
 
 def job():
-  i = args.start
+  i = args.init
   while True:
     print('[{}] start'.format(time.time()))
     r = s.request('get', args.url)
@@ -33,13 +37,17 @@ def job():
     i = i + args.step
 
 def job2():
-  for i in range(1):
+  i = args.job_init
+  while True:
     print(' [{}] start'.format(time.time()))
     r = s.request('get', args.url)
     print(' [{}] response code: {}'.format(time.time(), r.status_code))
-    time.sleep(1)
+    print('\n[{}] sleep {}'.format(time.time(), i))
+    time.sleep(i)
+    i = i + args.job_step
 
-# t = threading.Thread(target = job2)
-# t.start()
+if args.job_enable:
+  t = threading.Thread(target = job2)
+  t.start()
 
 job()

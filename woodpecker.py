fa7469e99fb73840e621e19b7c355bf4f4144150
TaopaiC <124380+TaopaiC@users.noreply.github.com>
2022-07-28 19:22:38 +0800

    REFACTOR: add tprint function

--- woodpecker.py
+++ woodpecker.py
@@ -14,11 +14,14 @@ parser.add_argument("--job-step", type=int, default=10, help="for 2nd job, inter
 args = parser.parse_args()
 print(args)
 
+def tprint(*args, **kwargs):
+  print('[{:.6f}] {}'.format(time.time(), ' '.join(map(str, args))))
+
 def patch_connectionpool():
   previous_get_conn = HTTPConnectionPool._get_conn
   def _new_get_conn(self, timeout=None):
     conn = previous_get_conn(self, timeout)
-    print('[{}] [q: {}/{}] conn.id: {}'.format(time.time(), self.pool.qsize(), self.pool.maxsize, id(conn)))
+    tprint('[q: {}/{}] conn.id: {}'.format(self.pool.qsize(), self.pool.maxsize, id(conn)))
     return conn
   HTTPConnectionPool._get_conn = _new_get_conn
 
@@ -29,20 +32,20 @@ s = requests.Session()
 def job():
   i = args.init
   while True:
-    print('[{}] start'.format(time.time()))
+    tprint('start')
     r = s.request('get', args.url)
-    print('[{}] response code: {}'.format(time.time(), r.status_code))
-    print('\n[{}] sleep {}'.format(time.time(), i))
+    tprint('response code: {}\n'.format(r.status_code))
+    tprint('sleep {}'.format(i))
     time.sleep(i)
     i = i + args.step
 
 def job2():
   i = args.job_init
   while True:
-    print(' [{}] start'.format(time.time()))
+    tprint(' start')
     r = s.request('get', args.url)
-    print(' [{}] response code: {}'.format(time.time(), r.status_code))
-    print('\n[{}] sleep {}'.format(time.time(), i))
+    tprint(' response code: {}\n'.format(r.status_code))
+    tprint(' sleep {}'.format(i))
     time.sleep(i)
     i = i + args.job_step
 
